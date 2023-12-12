76f7b5cced6ac3b39716bb59612768132b2ff4b0
Kirill Tregubov <contact@kirilltregubov.com>
2023-12-12 16:33:53 -0500

    Update Player.gd

--- scenes/level/Player.gd
+++ scenes/level/Player.gd
@@ -3,19 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var gravity = 0
-
 func _physics_process(delta):
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	var x_direction = Input.get_axis("move_left", "move_right")
 	if x_direction:
