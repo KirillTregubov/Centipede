8d1832c4f10a21b2104723c56d35b67adfe86a77
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2023-12-11 22:09:57 -0500

    start level

--- scenes/start/start.gd
+++ scenes/start/start.gd
@@ -1,11 +1,13 @@
 extends Control
 
+signal game_loaded()
+
 @export var transition_duration = 1.00
+@export var transition_ease = Tween.EASE_IN
 @export var transition_type = 1 # TRANS_SINE
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print_debug('aaaa')
 	pass # Replace with function body.
 
 
@@ -15,8 +17,14 @@ func _ready():
 
 
 func _on_start_game():
-	print_debug('start screen signal')
 	var tween = get_tree().create_tween()
-	tween.tween_property($MainMenuMusic,  "volume_db", -80, transition_duration).set_ease(Tween.EASE_IN).set_trans(transition_type)
+	tween.tween_property($MainMenuMusic,  "volume_db", -80, transition_duration).set_ease(transition_ease).set_trans(transition_type)
+	tween.tween_callback(_on_fade_out)
 	tween.play()
-	pass # Replace with function body.
+
+
+func _on_fade_out():
+	self.visible = false
+	emit_signal('game_loaded')
+	# TODO: figure out how this works
+	queue_free()
