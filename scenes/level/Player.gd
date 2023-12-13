575fe6641885fb1cea0907d92bcef6878e5c3429
Kirill Tregubov <contact@kirilltregubov.com>
2023-12-12 21:48:13 -0500

    text

--- scenes/level/Player.gd
+++ scenes/level/Player.gd
@@ -1,20 +1,22 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const ACCELERATION = 400.0
+const SPEED = 400.0
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var x_direction = Input.get_axis("move_left", "move_right")
 	if x_direction:
-		velocity.x = x_direction * SPEED
+		velocity.x += x_direction * ACCELERATION
 	else:
 		velocity.x = 0 #move_toward(velocity.x, 0, SPEED)
 	
 	var y_direction = Input.get_axis("move_up", "move_down")
 	if y_direction:
-		velocity.y = y_direction * SPEED
+		velocity.y = y_direction * ACCELERATION
 	else:
 		velocity.y = 0 #move_toward(velocity.y, 0, SPEED)
+	
+	velocity = velocity.limit_length(SPEED)
 
 	move_and_slide()
